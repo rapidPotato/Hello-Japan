0ed86de9765933f16cd9003ee10210ae0b3ec4dc
pollyj <55009539+pollyj@users.noreply.github.com>
2020-05-20 14:22:36 +0900

    update server.js

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log('server started at '+ port);
