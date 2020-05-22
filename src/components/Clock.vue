278cd380ed00e655e79c21ad51a0a21c195c323b
pollyj <55009539+pollyj@users.noreply.github.com>
2020-05-22 13:12:43 +0900

    update date to not be hard coded

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="Clock" class="container">
-      <div class="row">
-        <div class="col-6">
-            <p class="date align-content-left">May 22</p>
-        </div>
-        <div class="col-6">
-            <p class="time align-content-left" v-text="currentTime"></p>
-        </div>
+    <div class="row">
+      <div class="col-6">
+        <p class="date align-content-left" v-html="currentDate"></p>
       </div>
+      <div class="col-6">
+        <p class="time align-content-left" v-text="currentTime"></p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,39 +15,40 @@
 import moment from "moment";
 
 export default {
-  name: 'Clock',
+  name: "Clock",
   data() {
-      return {
-          message: 'Current Time:',
-          currentTime: null
-        }
-      },
-      methods: {
-          updateCurrentTime() {
-            this.currentTime = moment().format('LTS');
-          }
-        },
-        created() {
-          this.currentTime = moment().format('LTS');
-          setInterval(() => this.updateCurrentTime(), 1 * 1000);
-        }
-}
+    return {
+      message: "Current Time:",
+      currentTime: null,
+      currentDate: null
+    };
+  },
+  methods: {
+    updateCurrentTime() {
+      this.currentTime = moment().format("LTS");
+    }
+  },
+  created() {
+    this.currentTime = moment().format("LTS");
+    this.currentDate = moment().format(`MMMM D, YYYY (ddd)`);
+    setInterval(() => this.updateCurrentTime(), 1 * 1000);
+  }
+};
 </script>
 
 <style scoped>
 p {
-    margin-bottom: 5%;
-    color: rgb(74, 155, 155);
+  margin-bottom: 5%;
+  color: rgb(74, 155, 155);
 }
 
 #Clock {
-    font-size: 0.7em;
-    text-transform: uppercase;
-    font-weight: bold;
+  font-size: 0.7em;
+  text-transform: uppercase;
+  font-weight: bold;
 }
 
 .time {
-    margin-right: 10px;
+  margin-right: 10px;
 }
-
 </style>
